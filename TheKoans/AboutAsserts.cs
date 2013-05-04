c50c968899da1e0fa21cec94c187a7ee6384d4ee
John S. Ryan <jtigger@infosysengr.com>
2013-05-03 17:39:44 -0700

    Koans are individual test methods sequenced in an Ordered Test

---
+++ TheKoans/AboutAsserts.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TheKoans
+{
+
+    [TestClass]
+    public class AboutAsserts
+    {
+        public const int ReplaceMeWithAnInteger = 0;
+
+        [TestMethod]
+        public void AssertTruth()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void AssertWithMessage()
+        {
+            Assert.IsTrue(false, "This condition should be true, please make it so.");
+        }
+
+        [TestMethod]
+        public void AssertEquality()
+        {
+            var expectedValue = ReplaceMeWithAnInteger;
+            var actualValue = 1 + 1;
+
+            Assert.AreEqual(expectedValue, actualValue, "A lack of understanding about equality has broken your karma.");
+        }
+
+
+    }
+}
---
+++ TheKoans/AboutNull.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TheKoans
+{
+    [TestClass]
+    public class AboutNull
+    {
+        public const string ReplaceMeWithAString = "";
+
+        [TestMethod]
+        public void NullIsNotAnInstanceOfObjectAndInvokingAMethodThrowsNullReferenceException()
+        {
+            Object nullReference = null;
+
+            try
+            {
+                nullReference.GetHashCode();
+            }
+            catch (Exception exception)
+            {
+                Assert.AreEqual(ReplaceMeWithAString, exception.Message);
+            }
+        }
+    }
+}
