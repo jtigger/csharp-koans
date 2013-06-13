14cbffee4dcc6536e3934fdbbdd25453bec0ced4
pdmoore <pdmoore@gmail.com>
2013-06-13 09:26:51 -0700

    Finished AboutAsserts, updated pathtoenlightment with new koans.

--- TheKoans/AboutAsserts.cs
+++ TheKoans/AboutAsserts.cs
@@ -1,38 +1,57 @@
 ﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TheKoans
 {
-
     [TestClass]
     public class AboutAsserts
     {
-        public const int ReplaceMeWithAnInteger = 0;
+        private static readonly Int32 FILL_ME_IN = new Int32();
+        private static readonly bool FILL_ME__IN;
 
         [TestMethod]
         public void AssertTruth()
         {
-            Assert.IsTrue(false);
+            Assert.IsTrue(false);   // Your long journey begins with a simple step.  We seek what's true, help us find it.
         }
 
         [TestMethod]
-        public void AssertWithMessage()
+        public void AssertsShouldHaveMessages()
         {
-            Assert.IsTrue(false, "This condition should be true, please make it so.");
+            Assert.IsTrue(false, "This message should help you understand what failed. Please help us find truth.");
+        }
+
+        [TestMethod]
+        public void AssertFalse()
+        {
+            Assert.IsFalse(true, "Truth comes in many forms. Here what is true should be false.");
         }
 
         [TestMethod]
         public void AssertEquality()
         {
-            var expectedValue = ReplaceMeWithAnInteger;
-            var actualValue = 1 + 1;
-
-            Assert.AreEqual(expectedValue, actualValue, "A lack of understanding about equality has broken your karma.");
+            Assert.IsTrue(1 == FILL_ME_IN, "Truth can be uncovered in boolean expressions when they are true.");
         }
 
+        [TestMethod]
+        public void AssertEqualityTheBetterWay()
+        {
+            var expectedValue = FILL_ME_IN;
+            var actualValue = 1 + 1;
 
+            Assert.AreEqual(expectedValue, actualValue, "When your karma is broken it is more helpful to know what was expected and what it actually is.");
+        }
+
+        [TestMethod]
+        public void AssertFail()
+        {
+            bool thePathToEnlightment = false;
+            if (!thePathToEnlightment)
+            {
+                Assert.Fail("Taking an unfortunate code path breaks your karma. Change the path towards truth.");
+            }
+            
+            Assert.IsTrue(thePathToEnlightment, "The path has been found.");
+        }
     }
 }
