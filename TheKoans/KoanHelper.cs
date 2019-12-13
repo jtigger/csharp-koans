61d03491e2049420729028f77aa3062fd80d675c
Pedro Herrera <herrera.pedro@live.com>
2019-12-12 23:36:14 -0600

    Fixed collection assert error

--- TheKoans/KoanHelper.cs
+++ TheKoans/KoanHelper.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-
+using System.Collections;
 
 namespace TheKoans
 {
     public class KoanHelper
     {
         public static object FILL_ME_IN = new Object();
+        public static ICollection FILL_ME_IN2 = new ArrayList();
     }
 
     //This is just used when we need a type
